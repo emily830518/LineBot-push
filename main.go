739f8ef010e74304e9f83b5059a0af4b7d5f46e7
emily830518 <nrl@nrls-MacBook-Air.local>
2017-05-04 11:31:56 +0800

    change timer

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func periodicFunc(tick time.Time){
 }
 
 func main() {
-	ticker := time.NewTicker(1 * time.Minute)
+	ticker := time.NewTicker(5 * time.Second)
 	go func(){
         for t := range ticker.C {
             //Call the periodic function here.
